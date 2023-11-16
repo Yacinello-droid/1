d8c9385479f3169a9fabbef0bf3f510978aa28eb
Yacinello-droid <136171795+Yacinello-droid@users.noreply.github.com>
2023-11-17 02:47:11 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package IDK;
+import java.util.Scanner;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 0;
+
+        while (a < 10) {
+            a += 1;
+            System.out.println("hii");
+        }
+    }
+}
